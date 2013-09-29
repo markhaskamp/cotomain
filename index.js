a2e04e2af10c4dcc7456abc4142426c7baa0cb10
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-09-29 09:06:39 -0400

    one carousel

--- index.js
+++ index.js
@@ -2,15 +2,9 @@
 $(document).ready(function() {
 	$('.bxSliderLeft').bxSlider({
 									     auto: true,
-	                     mode: 'fade',
-	                     pause: 2000,
-					             pager: false
-								     });			
-
-	$('.bxSliderRight').bxSlider({
-									     auto: true,
 					             controls: false,
 					             pager: false
 								     });			
+
 });
 
