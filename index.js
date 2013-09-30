bfcdbcab34745b48308f3ebefda8e5fc8370f912
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-09-30 07:59:46 -0400

    mrh

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
 	$('.bxSliderLeft').bxSlider({
 									     auto: true,
+                       pause: 8000,
 					             controls: false,
 					             pager: false
 								     });			
