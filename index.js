b34b6af92c3b802899d71ae8eea0e96e2b97f535
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-10-02 21:16:48 -0400

    default pause before changing pictures in carousel

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
 	$('.bxSliderLeft').bxSlider({
 									     auto: true,
-                       pause: 8000,
 					             controls: false,
 					             pager: false
 								     });			
